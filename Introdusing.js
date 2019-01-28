c49f824787d318d7a246ae2c5e5df08c4bba18aa
pavel <p.sharapov1@gmail.com>
2019-01-28 11:20:39 +0200

    test commit

---
+++ Introdusing.js
@@ -0,0 +1,10 @@
+/*let e = 7+4;
+console.log(e);*/
+
+let drawCats = function (howManyTimes) {
+    for (i = 1; i<= howManyTimes; i++) {
+        console.log(i + ' =^.^=');
+    }
+
+};
+drawCats(10);
