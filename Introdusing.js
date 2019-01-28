4a119ce276a1452715f741c351e4586f22e20927
pavel <p.sharapov1@gmail.com>
2019-01-28 13:06:22 +0200

    the end of part 1 and 2

--- Introdusing.js
+++ Introdusing.js
@@ -1,10 +1,50 @@
-/*let e = 7+4;
-console.log(e);*/
+// Продолжение с начала главы 3 (Массивы и объекты)
 
+
+
+
+
+
+
+// Глава 1,2
+/*
+// 10 cat emoji
 let drawCats = function (howManyTimes) {
     for (i = 1; i<= howManyTimes; i++) {
         console.log(i + ' =^.^=');
     }
-
 };
-drawCats(10);
+drawCats(10);*/
+
+/*
+
+// seconds in the year
+
+let secInMin = 60;
+let minInHour = 60;
+let hoursInDay = 24;
+let daysInYear = 365;
+let myAge = 33;
+let secInHour = secInMin * minInHour;
+let secInDay = secInHour * hoursInDay;
+let secInYear = secInDay * daysInYear;
+let secInMyAge = secInYear * myAge;
+console.log('seconds in hour = ' + secInHour);
+console.log('seconds in day = ' + secInDay);
+console.log('seconds in the year = ' + secInYear );
+console.log('seconds in my age = ' + secInMyAge);
+*/
+
+/*
+let numb = 5;
+numb *= 6;
+console.log(numb);
+numb++;
+console.log(numb);
+numb++;
+console.log(numb);*!/
+
+let iAm = 'Pavel'.toUpperCase();
+
+console.log(iAm[2]);
+*/
